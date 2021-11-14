29eb2f0d94eedce1d01fd142ceaced9cb2d47e62
alvaro lopes <alvim.lopez@ gmail.com>
2021-11-13 22:12:41 -0300

    começando o projeto de uma calculadora em JavaScript

---
+++ script/controller/calcControlle.js
@@ -0,0 +1,26 @@
+class CalcController{
+    constructor(){
+
+        this._displayCalc = "0";
+        this._dataAtual;
+
+
+    }
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    set displayCalc (valor){
+        this._displayCalc = valor;
+    }
+    get _dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        return this._dataAtual = valor;
+    }
+
+
+
+
+}
